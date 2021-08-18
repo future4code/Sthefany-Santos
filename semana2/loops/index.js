29139b2b6c0f7791f5850baa56092a178de94cde
Sthefany <sthefany.gabriele@hotmail.com>
2021-08-18 16:22:11 -0300

    Atividade loops

---
+++ semana2/loops/index.js
@@ -0,0 +1,48 @@
+// let valor = 0
+// for(let i = 0; i < 5; i++) {
+//   valor += i
+// }
+// console.log(valor)
+// // ele erá executar até chegar em 5
+
+// const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+// for (let numero of lista) {
+//   if (numero > 18) {
+// 		console.log(numero)
+// 	}
+// }
+// // 18, 19, 21, 23, 25, 27, 30
+// // seria while, não for of
+
+// const quantidadeTotal = Number(prompt("Digite a quantidade de linhas: "))
+// let quantidadeAtual = 0
+// while(quantidadeAtual < quantidadeTotal){
+//   let linha = ""
+//   for(let asteriscos = 0; asteriscos < quantidadeAtual + 1; asteriscos++){
+//     linha += "*"
+//   }
+//   console.log(linha)
+//   quantidadeAtual++
+// }
+
+// // iria retornar até chegar em 4, chegando em cinco retornaria um *
+
+///------------------------------------------------------------------------
+// Exercícios de escrita de código
+
+// 1 - Pergunte ao usuário quantos bichinhos de estimação ele tem e guarde esse dado em uma variável. 
+
+const quantidadeBichinho = Number(prompt("Quantos bichinhos de estimação você tem?"))
+
+// a) Se a quantidade for 0, imprima no console "Que pena! Você pode adotar um pet!"
+
+
+
+// b) Se a quantidade for maior que 0, solicite que o usuário digite os nomes deles, um por um, e guarde esses nomes em um array
+
+
+
+//  Coloque o seu prompt dentro de um loop. Esse loop deve ser executado a mesma quantidade de vezes que o usuário inseriu. Por exemplo: se o usuário tem 4 pets, ele deve conseguir inserir 4 nomes.
+
+// c) Por fim, imprima o array com os nomes dos bichinhos no console
+
