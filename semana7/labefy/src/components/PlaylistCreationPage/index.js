78ae8528d6bbc77dc4d18f12b1a286687f929f62
Sthefany <sthefany.gabriele@hotmail.com>
2021-09-27 23:20:48 -0300

    Labefy incompleto

---
+++ semana7/labefy/src/components/PlaylistCreationPage/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components'
+
+const PlaylistCreationFormContainer = styled.div`
+  height: 500px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  
+`
+
+const PlaylistCreationForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const PlaylistCreationPage = () => {
+
+  return (
+    <PlaylistCreationFormContainer>
+      <PlaylistCreationPage>
+
+      </PlaylistCreationPage>
+    </PlaylistCreationFormContainer>
+  )
+}
+
+export default PlaylistCreationPage;
