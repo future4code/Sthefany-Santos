0b26456f38d903a8a7d2ae4ab1547b9c76b63ce6
SthefanyLara <sthefany.gabriele@hotmail.com>
2021-10-05 18:59:21 -0300

    aula31

--- semana9/aula31/insta-lab-hooks/insta-lab-hooks/src/App.js
+++ semana9/aula31/insta-lab-hooks/insta-lab-hooks/src/App.js
@@ -2,12 +2,32 @@ import React from 'react';
 import Post from './components/Post/Post';
 import styled from "styled-components"
 
+import React, { useState } from 'react'
+
+
 const AppContainer = styled.div `
   display: flex;
   justify-content: center;
   flex-direction: column;
   align-items: center;
 `
+
+
+const Post = (props) => {
+   const [curtido, setCurtido] = useState(false)
+
+   onChangeCurtido = (event) => {
+     setCurtido(event.target.value)
+   }
+
+   const [numeroCurtidas, setNumeroCurtidas] = useState("")
+
+   const [comentando, setComentando] = useComentando(false)
+
+   const [numeroComentarios, setNumeroComentarios] = useNumeroComentarios(+1)
+
+   const [comentarios, setComentarios] = useComentarios(false)
+}
 
 const App = () => {
   return (
